dc3fa7b12ef63d97f0e2c0e236763b93af8ba7ea
Martyn O'Connor <contact@martynoconnor.net>
2018-12-16 23:29:20 +0000

    Included more checks
    Included checks now for memory installed, CPU cores. Began the check of network with placeholder for ethtool.

--- inspect.sh
+++ inspect.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
+clear
+echo
+echo "##################################################"
+echo "# Splunk environment tester script for GNU/Linux #"
+echo "# Bult to check for any obvious problems in your #"
+echo "# environment and provide remedial guidance.     #"
+echo "##################################################"
+echo
 
+# Check for OS versions
 if [ -f /etc/os-release ]; then
-	. /etc/os-release
-	OS=$NAME
-	VER=$VERSION_ID
+        . /etc/os-release
+        OS=$NAME
+        VER=$VERSION_ID
 elif type lsb_release > /dev/null 2>&1; then
-	OS=$(lsb_release -si)
-	VER=$(lsb_release -sr)
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
 elif [ -f /etc/lsb-release ]; then
-	. /etc/lsb-release
-	OS=$DISTRIB_ID
-	VER=DISTRIB_RELEASE
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+        VER=DISTRIB_RELEASE
 elif [ -f /etc/debian_version ]; then
-	OS=Debian
-	VER=$(cat /etc/debian_version)
+        OS=Debian
+        VER=$(cat /etc/debian_version)
 fi
 printf "This is $OS Linux, version $VER\n"
+
+# Check for memory amounts
+memory_kb=$(cat /proc/meminfo | grep MemTotal: | awk '{print $2; }')
+printf "This machine has $memory_kb KB of memory\n"
+
+# Check for cpu cores
+cpu_cores=$(cat /proc/cpuinfo | grep -c "^processor")
+printf "This machine has $cpu_cores cores available.\n"
+
+# test for presence of ethtool for network speed checks
+...
