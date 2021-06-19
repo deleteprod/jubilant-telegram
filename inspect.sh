5106e4d66b1031ec89b5cacf3ddb364fd0dcc36f
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2021-06-19 15:34:09 +0100

    This breaks it, but I want to see where

--- inspect.sh
+++ inspect.sh
@@ -29,6 +29,13 @@ printf "This is $OS Linux, version $VER\n"
 # Check for memory amounts
 memory_kb=$(cat /proc/meminfo | grep MemTotal: | awk '{print $2; }')
 printf "This machine has $memory_kb KB of memory\n"
+if [ "$memory_kb" -lt 16777216 ]; then
+    echo "At 16GB or less, you're not likely to have a good time with Splunk"
+elif
+   [ "$memory_kb" -gt 33554432 ] then
+    echo "At leat 32GB, now we're talking"
+else
+    echo "Seems you're all good on memory dude"
 
 # Check for cpu cores count
 cpu_cores=$(cat /proc/cpuinfo | grep -c "^processor")
