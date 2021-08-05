8b6b2b9cac4a88f32626e6caadde6cfda1b6458d
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2021-08-05 09:30:18 +0100

    Reworked logging with comments and credit to the person who showed me it

--- scripts/inspect.sh
+++ scripts/inspect.sh
@@ -8,9 +8,19 @@ echo "# environment and provide remedial guidance.      #"
 echo "###################################################"
 echo
 
-# Enable logging
+# Enable logging - thanks to nicerobot - https://serverfault.com/users/18440/nicerobot
+
+# Saves file descriptors so they can be restored to whatever they were before 
+# redirection or used themselves to output to whatever they were before the following redirect.
 exec 3>&1 4>&2
+
+# Restore file descriptors for particular signals. Not generally necessary since they 
+# should be restored when the sub-shell exits.
 trap 'exec 2>&4 1>&3' 0 1 2 3
+
+# Redirect stdout to file log.out then redirect stderr to stdout. Note that the order is 
+# important when you want them going to the same file. stdout must be redirected before 
+# stderr is redirected to stdout.
 exec 1>../logging/jt.log 2>&1
 
 # Check for OS versions
